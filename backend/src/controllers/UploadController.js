f9dabc8a03dd8d340546774650b0a85e9793655c
Julien ichard WCS <julien.y.richard@gmail.com>
2022-06-21 15:53:59 +0200

    Cleaning the backend code

--- backend/src/controllers/UploadController.js
+++ backend/src/controllers/UploadController.js
@@ -2,13 +2,11 @@ const fs = require("fs");
 
 class UploadController {
   static upload = (req, res) => {
-    console.log(req.file);
     fs.rename(
       req.file.path,
       `public/images/${req.file.originalname}`,
       (err) => {
         if (err) {
-          console.log(err);
           res.status(400).send("Error while uploading");
         } else {
           res.status(203).json({
