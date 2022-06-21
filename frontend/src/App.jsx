b055ad82ab8b112531d444a5e903606c81b17a45
Julien ichard WCS <julien.y.richard@gmail.com>
2022-06-21 15:57:38 +0200

    Creating the submit function with data param and initialize the formData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import { useForm } from "react-hook-form";
 
 function App() {
   const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
